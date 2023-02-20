af66a4fc33863bc47955ee88ca80921d2e1f4680
Sara <ssevans@umich.edu>
2023-02-19 20:26:11 -0500

    updated spelling

--- money.js
+++ money.js
@@ -1,19 +1,19 @@
 (function (){
-    let moneyButton=document.getElementById("money-button");
+    const moneyButton=document.getElementById("money-button");
 
     moneyButton.onclick = () => {
-		let coinArea = document.getElementById("coins");
-		let amount = document.getElementById("how-many").value;
-		let coinTypes = document.getElementById("which-coin");
-		let coinType = coinTypes.options[coinTypes.selectedIndex].value;
+		const coinArea = document.getElementById("coins");
+		const amount = document.getElementById("how-many").value;
+		const coinTypes = document.getElementById("which-coin");
+		const coinType = coinTypes.options[coinTypes.selectedIndex].value;
 		for(let i = 0; i < amount; i++){
 			coinArea.append(makeCoin(coinType));
 		}
 	}
 	
 	function makeCoin(coinType) {
-		let coinContainer = document.createElement("div");
-		let coin = document.createElement("div");
+		const coinContainer = document.createElement("div");
+		const coin = document.createElement("div");
 		coin.classList.add("coin");
 		coin.classList.add(coinType);
 		coin.innerHTML = coinType;
